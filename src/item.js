5cbe8063ee3e07a17a37016acbaa01829e99ec60
cantstopcoding <jonathan_koss@yahoo.com>
2020-12-04 10:50:20 -0500

    make review appear on under reviews

--- src/item.js
+++ src/item.js
@@ -1,18 +1,19 @@
 class Item {
     constructor(item, itemAttributes) {
+        // debugger 
         this.id = item.id
         this.name = itemAttributes.name
         this.price = itemAttributes.price
         this.description = itemAttributes.description
         this.image_url = itemAttributes.image_url
         this.category = itemAttributes.category
+        this.reviews = itemAttributes.reviews.map(review => review.content).join(' ')
         Item.all.push(this)
         console.log(this)
     }
 
     renderItemCard() {
         // debugger source: https://www.youtube.com/watch?v=2xvuGWI3H58&t=349s 102:19
-        
         return `
             <div class="col-md-4">
                 <div class="card mb-4 shadow-sm">
@@ -40,12 +41,13 @@ class Item {
                             <form>
                                 <div class="form-group">
                                     <label for="exampleFormControlTextarea1">Write a Review... </label>
-                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                    <textarea class="form-control" id="create-review-form" rows="3"></textarea>
                                     <button class="btn btn-sm btn-outline-secondary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
                                         Submit
                                     </button>
                                 </div>
                             </form>
+                            ${this.reviews}
                         </div>
                     </div>
                 </div>
