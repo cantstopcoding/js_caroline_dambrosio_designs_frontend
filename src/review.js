5120afff663f3ba287c77b796f85c2b81aaf3721
cantstopcoding <jonathan_koss@yahoo.com>
2020-12-18 23:55:36 -0500

    add created at when submit on post

--- src/review.js
+++ src/review.js
@@ -3,6 +3,7 @@ class Review {
         this.id = reviewData.id
         this.content = reviewAttributes.content
         this.itemId = reviewAttributes.item_id
+        this.createdAt = reviewAttributes.created_at
         Review.all.push(this)
     }
     
@@ -10,7 +11,7 @@ class Review {
         return `
         <br>
         <div class="card border-dark mb-3" style="max-width: 18rem;">
-            <div class="card-header">Review Posted...</div>
+            <div class="card-header">Review Posted ${this.createdAt}</div>
             <div class="card-body text-dark">
                 <h6 class="card-title">${this.content}</h6>
                 <p class="card-text"></p>
