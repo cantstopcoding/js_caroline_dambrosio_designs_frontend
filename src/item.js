148587b5ed0b014e2fb984a7b4aed32b40d9d6cf
cantstopcoding <jonathan_koss@yahoo.com>
2021-06-01 14:13:16 -0400

    change spacing with html in item object

--- src/item.js
+++ src/item.js
@@ -19,45 +19,45 @@ class Item {
   renderItemCard() {
     // debugger source: https://www.youtube.com/watch?v=2xvuGWI3H58&t=349s 102:19
     return `
-            <div id="item-${this.id}">
-                <div class="col-md-4">
-                    <div class="card mb-4 shadow-sm">
-                        <img src="${this.image_url}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">${this.name}</h5>
-                            <p class="card-text">${this.description}</p>
-                            <p class="card-text">$${this.price}</p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                                    <button data-id=${this.id} type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                                </div>
-                                <small class="text-muted">Category: ${this.category.name}</small>
-                            </div>
-                        </div>
-                        <p>	                        
-                            <button id="reviews-button" class="btn btn-sm btn-outline-secondary" type="button" data-toggle="collapse" data-target="#collapseExample-${this.id}" aria-expanded="false" aria-controls="collapseExample-${this.id}">	                        
-                                Reviews	
-                            </button>	
-                        </p>
-                        <div class="collapse" id="collapseExample-${this.id}">
-                            <div class="card card-body">
-                                <form id="create-review-form">
-                                    <div class="form-group">
-                                        <label for="content">Write a Review...</label>
-                                        <textarea class="form-control" id="input-${this.id}" name="content" value="" rows="3"></textarea>
-                                    </div>
-                                    <button data-item-id="${this.id}" class="btn btn-sm btn-outline-secondary" type="submit" value="Submit">Submit</button>
-                                </form>
-                                <div class="container" id="review-container-${this.id}">
-                                    
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+        <div id="item-${this.id}">
+          <div class="col-md-4">
+            <div class="card mb-4 shadow-sm">
+              <img src="${this.image_url}" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">${this.name}</h5>
+                <p class="card-text">${this.description}</p>
+                <p class="card-text">$${this.price}</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                    <button data-id=${this.id} type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                  </div>
+                  <small class="text-muted">Category: ${this.category.name}</small> 
                 </div>
+              </div>
+              <p>	                        
+                <button id="reviews-button" class="btn btn-sm btn-outline-secondary" type="button" data-toggle="collapse" data-target="#collapseExample-${this.id}" aria-expanded="false" aria-controls="collapseExample-${this.id}">	                        
+                  Reviews	
+                </button>	
+              </p>
+              <div class="collapse" id="collapseExample-${this.id}">
+                <div class="card card-body">
+                  <form id="create-review-form">
+                    <div class="form-group">
+                      <label for="content">Write a Review...</label>
+                      <textarea class="form-control" id="input-${this.id}" name="content" value="" rows="3"></textarea>
+                    </div>
+                    <button data-item-id="${this.id}" class="btn btn-sm btn-outline-secondary" type="submit" value="Submit">Submit</button>
+                  </form>
+                  <div class="container" id="review-container-${this.id}">
+                      
+                  </div>
+                </div>
+              </div>
             </div>
-        `;
+          </div>
+        </div>
+      `;
   }
 
   clickEventForAllReviews() {
@@ -66,7 +66,6 @@ class Item {
         cb.getElementsByTagName("button")[
           cb.getElementsByTagName("button").length - 1
         ];
-
       reviewSubmitButton.addEventListener("click", (e) => {
         reviewFormHandler(e);
       });
@@ -75,15 +74,15 @@ class Item {
 
   renderReviewContent(review) {
     return `
-        <br>
-        <div class="card border-dark mb-3" style="max-width: 18rem;">
-            <div class="card-header">Review Posted ${review.created_at}</div>
-            <div class="card-body text-dark">
-                <h6 class="card-title">${review.content}</h6>
-                <p class="card-text"></p>
-            </div>
+      <br>
+      <div class="card border-dark mb-3" style="max-width: 18rem;">
+        <div class="card-header">Review Posted ${review.created_at}</div>
+        <div class="card-body text-dark">
+          <h6 class="card-title">${review.content}</h6>
+          <p class="card-text"></p>
         </div>
-        `;
+      </div>
+    `;
   }
 }
 
